9fda196ee209c1e1b75bc8a72c6be1e7ef396899
pasandevin <jayawardene.pasandevin@gmail.com>
2022-05-04 11:52:20 +0530

    simple test to test activity

--- app/src/test/java/com/pasandevin/robolectrictester/ExampleUnitTest.kt
+++ app/src/test/java/com/pasandevin/robolectrictester/ExampleUnitTest.kt
@@ -1,17 +1,37 @@
 package com.pasandevin.robolectrictester
 
-import org.junit.Test
 
+import android.content.Intent
+import android.widget.TextView
 import org.junit.Assert.*
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.Robolectric
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.RuntimeEnvironment
+
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
+@RunWith(RobolectricTestRunner::class)
 class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
     }
+
+    @Test
+    fun check_main_activity() {
+        val activity: MainActivity = Robolectric.setupActivity(MainActivity::class.java)
+        val textView : TextView = activity.findViewById<TextView>(R.id.textView)
+        assertEquals("Hello World!", textView.text)
+    }
+
+
+
+
+
 }
