fbea3cdff8979d30de914c58352efb3153d3e57a
pasandevin <jayawardene.pasandevin@gmail.com>
2022-05-04 13:40:24 +0530

    simple test for fragment

--- app/src/test/java/com/pasandevin/robolectrictester/ExampleUnitTest.kt
+++ app/src/test/java/com/pasandevin/robolectrictester/ExampleUnitTest.kt
@@ -1,14 +1,14 @@
 package com.pasandevin.robolectrictester
 
 
-import android.content.Intent
 import android.widget.TextView
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 import org.junit.Assert.*
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.Robolectric
 import org.robolectric.RobolectricTestRunner
-import org.robolectric.RuntimeEnvironment
 
 
 /**
@@ -23,8 +23,28 @@ class ExampleUnitTest {
     @Test
     fun check_main_activity() {
         val activity = Robolectric.buildActivity(MainActivity::class.java).create().get()
-        val textView : TextView = activity.findViewById<TextView>(R.id.textView)
+        val textView : TextView = activity.findViewById<TextView>(R.id.text_activity)
         assertEquals("Hello World!", textView.text)
+    }
+
+    @Test
+    fun check_first_fragment(){
+        val fragment = FirstFragment()
+        val activity = Robolectric.buildActivity(
+            SecondActivity::class.java
+        )
+            .create()
+            .start()
+            .resume()
+            .get()
+
+        val fragmentManager: FragmentManager = activity.supportFragmentManager
+        val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.add(fragment, null)
+        fragmentTransaction.commit()
+
+        val textView : TextView = activity.findViewById<TextView>(R.id.text_fragment)
+        assertEquals("hello first fragment", textView.text)
     }
 
 
